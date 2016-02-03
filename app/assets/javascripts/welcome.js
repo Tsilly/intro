ad85fa4cad3d8c8ff49755855c65fcdaac43069b
Jill Li <jill.li87@gmail.com>
2016-02-03 17:25:07 +0100

    styling

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,9 +1,8 @@
 $(function(){
   var subtitle = $("#head-jumbotron-subtitle");
-  $('.head-jumbotron').hover(function(){
+  $('.jumbotron').hover(function(){
     subtitle.html("Give me 5 minutes to introduce myself.")
   }, function() {
     subtitle.html("A Web Developer, Coffee Lover & Traveller.")
-
   });
 });
