d4e14af2a247e553d5e55dd72e787ece675274ac
Jill Li <jill.li87@gmail.com>
2016-02-04 15:00:43 +0100

    styling & animation

--- Gemfile
+++ Gemfile
@@ -3,9 +3,9 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
+gem 'bootstrap-sass', '~> 3.3.6'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
-gem 'bootstrap-sass', '~> 3.3.6'
 gem "autoprefixer-rails"
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,8 +1,13 @@
 $(function(){
-  var subtitle = $("#head-jumbotron-subtitle");
+  var title = $(".jumbotron div h1");
+  var subtitle = $(".jumbotron div h3");
+  var button = $(".jumbotron div a");
   $('.jumbotron').hover(function(){
-    subtitle.html("Give me 5 minutes to introduce myself.")
-  }, function() {
-    subtitle.html("A Web Developer, Coffee Lover & Traveller.")
+    title.animate({
+      height: '200px',
+      width: '200px'
+    }, function(){
+      title.css({"background-color": "#888888", "opacity": "0.8"});
+    });
   });
 });
