8bfb957fb73ed2777404c6871e2f92ffb4bd2436
Jill Li <jill.li87@gmail.com>
2016-02-08 11:30:10 +0100

    styling

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,12 +3,15 @@ $(function(){
   var subtitle = $(".jumbotron div h3");
   var button = $(".jumbotron div a");
   $('.jumbotron').hover(function(){
+
     title.animate({
-      height: '200px',
-      width: '200px'
+      width: '200px',
+      opacity: 0.6
     }, function(){
       title.css({"background-color": "#484848", "opacity": "0.8", color: "#F8F8F8"});
       subtitle.css({"color": "#F8F8F8"});
-    });
+    }
+
+    );
   });
 });
